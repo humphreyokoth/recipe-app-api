8e8105fb9639acd4cff30a3e747ba48a9293fb29
humphreyokoth <humpasp@gmail.com>
2023-05-31 12:48:58 +0300

    Feat:test for db connection

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -19,5 +19,17 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db_ready')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Testing waiting for db when getting Operational error"""
+
+        patched_check.side_effect = [Psycopg2Error] *2 +\
+            [OperationalError] * 3 + [True]
+        
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
+        
